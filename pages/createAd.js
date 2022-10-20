89995917dcb55891542b07d099f199a67905502c
dorjoo <dorjoohover@gmail.com>
2022-10-21 03:41:50 +0800

    asdf

---
+++ pages/createAd.js
@@ -0,0 +1,15 @@
+import { Grid, VStack } from "@chakra-ui/react";
+
+
+
+const categories = []
+
+export default function CreateAd () {
+    return (
+        <VStack>
+            <Grid>
+                
+            </Grid>
+        </VStack>
+    )
+}
