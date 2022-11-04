0abb3a27bec165653cb3a05a673f4e7073eec6eb
fl1pHover <bishu.hover@gmail.com>
2022-11-05 03:51:35 +0800

    asd

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
-// import AdContent from "../components/home/adContent";
-// import CategorySelect from "../components/home/categorySelect";
-// import SwiperHeader from "../components/home/swiperHeader";
+import AdContent from "../components/home/adContent";
+import CategorySelect from "../components/home/categorySelect";
+import SwiperHeader from "../components/home/swiperHeader";
 import ScrollTop from "../util/scrollTop";
 
-import dynamic from "next/dynamic";
-const SwiperHeader = dynamic(() => import("../components/home/swiperHeader"));
-const CategorySelect = dynamic(() =>
-     import("../components/home/categorySelect")
-);
-const AdContent = dynamic(() => import("../components/home/adContent"));
+// import dynamic from "next/dynamic";
+// const SwiperHeader = dynamic(() => import("../components/home/swiperHeader"));
+// const CategorySelect = dynamic(() =>
+//      import("../components/home/categorySelect")
+// );
+// const AdContent = dynamic(() => import("../components/home/adContent"));
 
 export default function Home() {
      const [products, setProducts] = useState("");
