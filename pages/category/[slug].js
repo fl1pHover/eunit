b5c4eac12f9249650be9217167823b8b72ade569
fl1pHover <bishu.hover@gmail.com>
2023-01-10 14:33:37 +0800

    category zaruud

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,54 +1,83 @@
-import { Box, Grid, Heading, Image, Stack } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import MainContainer from "../../layout/mainContainer";
+import { Box, Grid, GridItem, Heading, Image, Stack } from '@chakra-ui/react';
+import MainContainer from '../../layout/mainContainer';
 
-import FilterLayout from "../../components/filter";
-import { useRouter } from "next/router";
-import urls from "../../constants/api";
+import { useRouter } from 'next/router';
+import FilterLayout from '../../components/filter';
 
 const Category = () => {
-    
-     const router = useRouter()
-   
-     return (
-          <Box my={5} as="section" id="category">
-               <MainContainer>
-                    <Stack direction={"row"} py={2} gap={3}>
-                         {/* //TODO Filter Box */}
-                         {router.query?.slug && <FilterLayout data={router.query.slug} />}
+  const router = useRouter();
 
-                         {/* //TODO Filter box end */}
+  return (
+    <Box my={5} as="section" id="category">
+      <MainContainer>
+        <Stack direction={'row'} py={2} gap={3}>
+          {/* //TODO Filter Box */}
+          {router.query?.slug && <FilterLayout data={router.query.slug} />}
 
-                         {/* //TODO Main product */}
-                         <Box
-                              maxWidth={"75%"}
-                              flex="0 0 75%"
-                              borderRadius="5px"
-                         >
-                              {/* <SwiperHeader /> */}
-                              <Image
-                                   src="/images/HeaderSlider/1.jpg"
-                                   height={"400px"}
-                                   width="100%"
-                                   objectFit={"cover"}
-                                   alt="image"
-                              />
-                              <Heading variant={"mediumHeading"} my={3}>
-                                   Онцгой зар
-                              </Heading>
-                              <Grid
-                                   direction={"row"}
-                                   templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-                                   rowGap={5}
-                                   gap={3}
-                                   width="100%"
-                                   justifyContent={"center"}
-                              ></Grid>
-                         </Box>
-                    </Stack>
-               </MainContainer>
+          {/* //TODO Filter box end */}
+
+          {/* //TODO Main product */}
+          <Box maxWidth={'75%'} flex="0 0 75%" borderRadius="5px">
+            {/* <SwiperHeader /> */}
+            <Image
+              src="/images/HeaderSlider/1.jpg"
+              height={'400px'}
+              width="100%"
+              objectFit={'cover'}
+              alt="image"
+            />
+
+            {/* //TODO Ontsgoi zar */}
+            {/* //TODO Ontsgoi zar */}
+            {/* //TODO Ontsgoi zar */}
+
+            <Box>
+              <Heading variant={'mediumHeading'} my={3}>
+                Онцгой зар
+              </Heading>
+              <Grid
+                direction={'row'}
+                templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+                rowGap={5}
+                gap={3}
+                width="100%"
+                justifyContent={'center'}
+              >
+                {/* йыбйыб */}
+                {/* йыбйыб */}
+                {/* йыбйыб */}
+              </Grid>
+            </Box>
+
+            {/* //TODO Engiin zar */}
+            {/* //TODO Engiin zar */}
+            {/* //TODO Engiin zar */}
+
+            <Box>
+              <Heading variant={'mediumHeading'} my={3}>
+                Зарууд
+              </Heading>
+              <Grid
+                direction={'row'}
+                templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+                rowGap={5}
+                gap={3}
+                width="100%"
+                justifyContent={'center'}
+              >
+                <GridItem>asdasd</GridItem>
+                <GridItem>asdasd</GridItem>
+                <GridItem>asdasd</GridItem>
+                <GridItem>asdasd</GridItem>
+                <GridItem>asdasd</GridItem>
+                <GridItem>asdasd</GridItem>
+              </Grid>
+            </Box>
           </Box>
-     );
+        </Stack>
+      </MainContainer>
+    </Box>
+  );
 };
 
 export default Category;
