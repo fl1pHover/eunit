8585f5286d09d402ee2dbce9b770f8e3f4b8806a
dorjoo <dorjoohover@gmail.com>
2023-04-20 02:15:43 +0800

    asdf'

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       'https://bom-file.s3.us-east-1.amazonaws.com',
       'bom-file.s3.us-east-1.amazonaws.com',
     ],
   },
-
-  //  webpack: (config, { isServer }) => {
-  //       if (!isServer) {
-  //           // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-  //           config.resolve.fallback = {
-  //               fs: false
-  //           }
-  //       }
-
-  //       return config;
-  //   }
 };
 
 module.exports = nextConfig;
