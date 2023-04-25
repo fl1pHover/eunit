8764d6f2d9808d037736b0ec260463063724bbe2
fl1p <b21fa1739@ufe.edu.mn>
2023-04-26 02:49:48 +0800

    asd

--- pages/walletPage.js
+++ pages/walletPage.js
@@ -1,20 +1,20 @@
-import urls from '@/constants/api';
-import mergeNames from '@/util/mergeNames';
-import { Heading, Image, useToast } from '@chakra-ui/react';
-import axios from 'axios';
-import { getCookie } from 'cookies-next';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { STYLES } from '../styles';
+import urls from "@/constants/api";
+import mergeNames from "@/util/mergeNames";
+import { Heading, Image, useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { STYLES } from "../styles";
 
 const WalletPage = ({ user }) => {
   const [point, setPoint] = useState({
-    email: '',
-    point: '',
-    message: '',
+    email: "",
+    point: "",
+    message: "",
   });
   const toast = useToast();
-  const token = getCookie('token');
+  const token = getCookie("token");
   const router = useRouter();
   const sendPoint = async () => {
     try {
@@ -22,38 +22,38 @@ const WalletPage = ({ user }) => {
         await axios
           .get(
             `${
-              urls['test']
+              urls["test"]
             }/user/point/${point.email.toLowerCase()}/${parseFloat(
               point.point
             )}/default/{message}?message=${point.message}`,
             {
               headers: {
                 Authorization: `Bearer ${token}`,
-                'Access-Control-Allow-Headers': '*',
+                "Access-Control-Allow-Headers": "*",
               },
             }
           )
           .then((d) => {
-            if (d.data.message == 'success') {
+            if (d.data.message == "success") {
               toast({
-                title: 'Амжилттай илгээлээ.',
-                status: 'success',
+                title: "Амжилттай илгээлээ.",
+                status: "success",
                 duration: 1000,
                 isClosable: true,
               });
             }
-            if (d.data.message == 'not found receiver') {
+            if (d.data.message == "not found receiver") {
               toast({
-                title: 'Хүлээн авагч олдсонгүй',
-                status: 'warning',
+                title: "Хүлээн авагч олдсонгүй",
+                status: "warning",
                 duration: 1000,
                 isClosable: true,
               });
             }
-            if (d.data.message == 'not enough points') {
+            if (d.data.message == "not enough points") {
               toast({
-                title: 'Үлдэгдэл хүрэлцэхгүй байна',
-                status: 'warning',
+                title: "Үлдэгдэл хүрэлцэхгүй байна",
+                status: "warning",
                 duration: 1000,
                 isClosable: true,
               });
@@ -65,56 +65,70 @@ const WalletPage = ({ user }) => {
   };
 
   return (
-    <div className={mergeNames('grid xl:grid-cols-2 grid-cols-1 gap-10 mt-5')}>
-      {/* Card */}
-      <div className="flex flex-col">
-        <div className="font-bold w-[400px] h-[250px] bg-gradient-to-r from-cyan-600 to-blue-700 rounded-lg p-5 flex flex-col justify-between mx-auto text-white">
-          <div className="flex justify-between">
-            <h1 className="">BOM хэтэвч</h1>
-            <Image
-              alt="bom logo"
-              src="/images/logo/bom-white.png"
-              className="w-[30px]"
-            />
-          </div>
-          <h1 className="">Нэр</h1>
-          <div>
-            <h1 className="text-white/80">Үлдэгдэл</h1>
-            <h1 className="text-xl">{user?.point ?? 0} ₮</h1>
+    <div className="flex flex-col gap-10">
+      <div
+        className={mergeNames("grid xl:grid-cols-2 grid-cols-1 gap-10 mt-5")}
+      >
+        {/* Card */}
+        <div className="flex flex-col">
+          <div className="font-bold w-full max-w-[450px] h-[250px] bg-gradient-to-r from-cyan-600 to-blue-700 rounded-lg p-5 flex flex-col justify-between mx-auto text-white">
+            <div className="flex justify-between">
+              <h1 className="">BOM хэтэвч</h1>
+              <Image
+                alt="bom logo"
+                src="/images/logo/bom-white.png"
+                className="w-[30px]"
+              />
+            </div>
+            <h1 className="">Нэр</h1>
+            <div>
+              <h1 className="text-white/80">Үлдэгдэл</h1>
+              <h1 className="text-xl">{user?.point ?? 0} ₮</h1>
+            </div>
           </div>
         </div>
-        <div className="grid flex-col grid-cols-2 gap-2 mx-auto mt-5">
+        <div className="flex flex-col w-full max-w-[450px] gap-2 mx-auto">
           <input
             placeholder="Шилжүүлэх хүний и-мэйл"
-            className={mergeNames(STYLES.input)}
+            className={mergeNames(STYLES.input, "w-full")}
             onChange={(e) => {
               setPoint((prev) => ({ ...prev, email: e.target.value }));
             }}
+            required
           />
+
           <input
             placeholder="Дүн"
-            className={mergeNames(STYLES.input)}
+            className={mergeNames(STYLES.input, "w-full")}
             onChange={(e) => {
               setPoint((prev) => ({ ...prev, point: e.target.value }));
             }}
+            required
           />
+
           <textarea
             placeholder="Мэссэж"
-            maxLength={100}
-            className={mergeNames(STYLES.input, 'rounded-md col-span-full')}
+            maxLength={200}
+            className={mergeNames(STYLES.input, "rounded-md col-span-full")}
             onChange={(e) => {
               setPoint((prev) => ({ ...prev, message: e.target.value }));
             }}
+            required
           />
-          <button
-            className={mergeNames(STYLES.blueButton, 'col-span-full p-2')}
-            onClick={() => sendPoint()}
-          >
-            Шилжүүлэх
-          </button>
+          <div>
+            <button
+              className={mergeNames(
+                STYLES.blueButton,
+                "col-span-full w-full p-2"
+              )}
+              onClick={() => sendPoint()}
+            >
+              Шилжүүлэх
+            </button>
+          </div>
         </div>
       </div>
-      <div className="">
+      <div className="w-full max-w-[800px]  mx-auto">
         <Heading variant="mediumHeading">Гүйлгээний түүх</Heading>
         <div className="flex flex-col w-full gap-3 mt-4">
           <div className="flex justify-between w-full">
@@ -138,7 +152,7 @@ const WalletPage = ({ user }) => {
                   {ph.receiver?.username}
                 </button>
                 <p className="font-bold text-right text-blue-700">
-                  {ph.type == 'sender' ? '-' : '+'}
+                  {ph.type == "sender" ? "-" : "+"}
                   {ph.point}
                 </p>
               </div>
