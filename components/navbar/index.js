533c2db505e4572e83b0929ed4a1aa6d63c838ba
dorjoo <dorjoohover@gmail.com>
2023-04-27 05:58:57 +0800

    'asdf'

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,11 +1,28 @@
 import { useState } from 'react';
 
 import { categories } from '@/data/categories';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+import urls from '../../constants/api';
 import CategoryBottom from './bottom';
 import UpperNav from './upper';
-
-const Navbar = ({user}) => {
+const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [user, setUser] = useState();
+  const token = getCookie('token');
+  const getUser = async () => {
+    await axios
+      .get(`${urls['test']}/user/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Access-Control-Allow-Headers': '*',
+        },
+      })
+      .then((d) => setUser(d.data));
+  };
+  useEffect(() => {
+    if (token) getUser();
+  }, [token]);
   // const { categories } = useAuth();
   // useEffect(() => {
   //   const handleScroll = () => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { AuthProvider } from "context/auth";
-import { AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { ChakraProvider } from '@chakra-ui/react';
+import { AuthProvider } from 'context/auth';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
-import AdminBar from "@/components/admin/AdminBar";
-import Navbar from "@/components/navbar";
-import urls from "@/constants/api";
-import Layout from "@/layout/layout";
-import { MainLoader } from "@/lib/Loader";
-import ScrollTop from "@/lib/ScrollTop";
-import theme from "@/lib/theme";
-import "@/styles/globals.scss";
-import axios from "axios";
-import { getCookie } from "cookies-next";
-import Router, { useRouter } from "next/router";
-import { useEffect } from "react";
-
+import Layout from '@/layout/layout';
+import { MainLoader } from '@/lib/Loader';
+import ScrollTop from '@/lib/ScrollTop';
+import theme from '@/lib/theme';
+import '@/styles/globals.scss';
+import Router, { useRouter } from 'next/router';
+import AdminBar from '../components/admin/AdminBar';
+import Navbar from '../components/navbar';
 function MyApp({ Component, pageProps }) {
   let [isLoading, setIsLoading] = useState(false);
 
@@ -24,24 +19,10 @@ function MyApp({ Component, pageProps }) {
     setIsLoading(false);
   };
   const router = useRouter();
-  Router.events.on("routeChangeStart", handleStart);
-  Router.events.on("routeChangeComplete", handleComplete);
-  Router.events.on("routeChangeError", handleComplete);
-  const [user, setUser] = useState();
-  const token = getCookie("token");
-  const getUser = async () => {
-    await axios
-      .get(`${urls["test"]}/user/me`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Access-Control-Allow-Headers": "*",
-        },
-      })
-      .then((d) => setUser(d.data));
-  };
-  useEffect(() => {
-    if (token) getUser();
-  }, [token]);
+  Router.events.on('routeChangeStart', handleStart);
+  Router.events.on('routeChangeComplete', handleComplete);
+  Router.events.on('routeChangeError', handleComplete);
+
   return (
     <AuthProvider>
       <ChakraProvider theme={theme}>
@@ -49,10 +30,10 @@ function MyApp({ Component, pageProps }) {
           <Layout>
             {isLoading && <MainLoader />}
             {/* <p>{JSON.stringify(Router)}</p> */}
-            {router?.pathname.substring(0, 6) == "/admin" ? (
+            {router?.pathname.substring(0, 6) == '/admin' ? (
               <AdminBar />
             ) : (
-              <Navbar user={user} />
+              <Navbar />
             )}
 
             <Component {...pageProps} />
