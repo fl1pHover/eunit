36d08d783e0d45bdf3275575e664042d3daab98b
fl1pHover <bishu.hover@gmail.com>
2023-04-27 03:53:42 +0800

    adadsasd

--- layout/layout.js
+++ layout/layout.js
@@ -1,20 +1,20 @@
-import CompareSelect from '@/components/Profile/CompareSelect';
-import urls from '@/constants/api';
-import { useAuth } from '@/context/auth';
-import axios from 'axios';
-import { getCookie, setCookie } from 'cookies-next';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import Footer from '../components/footer/index';
+import CompareSelect from "@/components/Profile/CompareSelect";
+import urls from "@/constants/api";
+import { useAuth } from "@/context/auth";
+import axios from "axios";
+import { getCookie, setCookie } from "cookies-next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Footer from "../components/footer/index";
 
 const Layout = ({ children }) => {
   const { comparison } = useAuth();
-  const comparisonCategory = getCookie('comparisonCategory');
+  const comparisonCategory = getCookie("comparisonCategory");
   const [ads, setAds] = useState([]);
   const getAds = async () => {
     await axios
-      .post(`${urls['test']}/ad/many/0/false`, comparison)
+      .post(`${urls["test"]}/ad/many/0/false`, comparison)
       .then((d) => setAds(d.data.ads));
   };
   useEffect(() => {
@@ -22,14 +22,14 @@ const Layout = ({ children }) => {
       getAds();
     } else {
       setAds([]);
-      setCookie('comparisonCategory', '');
+      setCookie("comparisonCategory", "");
     }
   }, [comparison]);
   const router = useRouter();
   return (
     <>
       <Head>
-        <title>BOM</title>
+        <title>E-unit</title>
         <meta name="description" content="Bom, zariin site" />
         <link rel="icon" href="/images/logo/bom-blue.png" />
       </Head>
@@ -41,13 +41,13 @@ const Layout = ({ children }) => {
         {children}
         {comparison &&
           ads &&
-          (router?.pathname == '/' ||
-            router?.pathname == '/category' ||
-            router?.pathname == '/category/[slug]' ||
-            router?.pathname == '/account/[slug]' ||
-            (router?.pathname == '/account' &&
-              (router?.query?.tab == 'MyAds' ||
-                router?.query?.tab == 'Bookmark'))) && (
+          (router?.pathname == "/" ||
+            router?.pathname == "/category" ||
+            router?.pathname == "/category/[slug]" ||
+            router?.pathname == "/account/[slug]" ||
+            (router?.pathname == "/account" &&
+              (router?.query?.tab == "MyAds" ||
+                router?.query?.tab == "Bookmark"))) && (
             <CompareSelect btnView={false} compareAds={ads} />
           )}
       </div>
