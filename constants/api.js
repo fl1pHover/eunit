dc647d8b6eb249086580a66107bbdcadaa401bf9
fl1pHover <bishu.hover@gmail.com>
2023-05-22 15:24:55 +0800

    changed api url

--- constants/api.js
+++ constants/api.js
@@ -1,6 +1,6 @@
 let urls = {
-  // test: `https://eunitbackend-production.up.railway.app`,
-  test: `http://localhost:5050`,
+  test: `https://eunitbackend-production.up.railway.app`,
+  // test: `http://localhost:5050`,
 
   // test: `192.168.1.49:5050`,
   production: ``,
