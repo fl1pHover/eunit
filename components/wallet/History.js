7c75c7e80b5b8ccc1be19788d0dc046aaf39dd1b
fl1pHover <bishu.hover@gmail.com>
2023-06-26 14:55:52 +0800

    wallet history

--- components/wallet/History.js
+++ components/wallet/History.js
@@ -8,11 +8,11 @@ const WHistory = ({ body, user }) => {
   return (
     <div className="w-full max-w-[800px] text-sm xl:text-base px-2 py-2 md:px-6 md:py-4 shadow-md border-2 border-slate-200/30 rounded-xl mx-auto">
       <Heading variant="mediumHeading">Гүйлгээний түүх</Heading>
-      <div className="flex flex-col w-full gap-3 mt-4">
+      {/* <div className="flex flex-col w-full gap-3 mt-4">
         <div className="flex items-center gap-4">
           <p className="font-semibold">№</p>
           <div className={mergeNames(colSpan, " text-gray-500")}>
-            {/* <p className="text-left ">Илгээгч</p> */}
+             <p className="text-left ">Илгээгч</p>
             <div className="flex flex-col justify-start text-left">
               <p className="">Мэссэж</p>
               <p className="text-sm">Хүлээн авагч</p>
@@ -20,33 +20,51 @@ const WHistory = ({ body, user }) => {
             <p className="text-right text-blue-700">E-unit</p>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="flex flex-col w-full mt-4">
         {user?.pointHistory?.map((ph, i) => {
           return (
-            <div className="flex flex-col w-full gap-3 mt-4" key={i}>
-              <div className="flex items-center gap-4">
-                <p className="font-semibold">{i + 1}</p>
-                <div className={mergeNames(colSpan, " text-gray-500")}>
-                  {/* <p className="text-left ">Илгээгч</p> */}
-                  <div className="flex flex-col justify-start text-left">
-                    <p className=""> {ph.message}</p>
-
-                    <a
-                      href={"/account/" + ph.receiver?.id}
-                      target="_blank"
-                      className={mergeNames(txthover, "text-center, text-sm")}
-                    >
-                      {ph.receiver?.username}
-                    </a>
+            <>
+              <div className="flex flex-col w-full gap-3 mt-4" key={i}>
+                <div className="flex items-center gap-4">
+                  <p className="font-semibold">{i + 1}</p>
+                  <div className={mergeNames(colSpan, " text-gray-500")}>
+                    {/* <p className="text-left ">Илгээгч</p> */}
+                    <div className="flex flex-col justify-start text-left">
+                      <p className=""> {ph.message}</p>
+                      {ph.type == "sender" ? (
+                        <div className="flex gap-2 text-sm font-normal">
+                          <p>to</p>
+                          <a
+                            href={"/account/" + ph.receiver?.id}
+                            target="_blank"
+                            className={mergeNames(txthover, "text-center")}
+                          >
+                            {ph.receiver?.username}
+                          </a>
+                        </div>
+                      ) : (
+                        <div className="flex gap-2 text-sm font-normal">
+                          <p>from</p>
+                          <a
+                            href={"/account/" + ph.sender?.id}
+                            target="_blank"
+                            className={mergeNames(txthover)}
+                          >
+                            {ph.sender?.username}
+                          </a>
+                        </div>
+                      )}
+                    </div>
+                    <p className="text-right text-blue-700">
+                      {ph.type == "sender" ? "-" : "+"}
+                      {ph.point}
+                    </p>
                   </div>
-                  <p className="text-right text-blue-700">
-                    {ph.type == "sender" ? "-" : "+"}
-                    {ph.point}
-                  </p>
                 </div>
               </div>
-            </div>
+              <div className="w-full h-[2px] bg-bgGrey mt-2" />
+            </>
             // <div
             //   className="flex items-center gap-4 border-t-2 border-gray-200"
             //   key={i}
